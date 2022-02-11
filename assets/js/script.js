9fd36e95d2148aa34e4dff421faeef8806fe5723
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-02-11 10:38:15 -0600

    cleaned up display next launch function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,4 @@
 const DateTime = luxon.DateTime;
-var upcomingEl = document.querySelector("#upcoming");
-var launchSitesEl = document.querySelector("#launch-sites");
-
 var launchSitesArr = [];
 
 var nextLaunchData = function () {
@@ -10,15 +7,8 @@ var nextLaunchData = function () {
     if (response.ok) {
       response.json().then(function (data) {
 
-        console.log(data);
-
-        var index = 0;
-
-        // if (data[index].launch_library_id) {
-        //   index++;
-        // }
-
-        displayNextLaunch(data[index]);
+        displayNextLaunch(data[0]);
+        // console.log(data);
       });
 
     } else {
@@ -31,85 +21,38 @@ var nextLaunchData = function () {
 
 var displayNextLaunch = function (data) {
 
-  var date = data.date_local;
+  var date = DateTime.fromISO(data.date_local).toLocaleString(DateTime.DATETIME_SHORT);
+  var siteId = data.launchpad;
+  var siteUrl = "https://api.spacexdata.com/v4/launchpads/" + siteId;
+  var payloadUrl = "https://api.spacexdata.com/v4/payloads/" + data.payloads[0];
   var flight = data.flight_number;
   var reddit = data.links.reddit.campaign;
+  var siteImg = "No Image Available";
+  for (var i = 0; i < launchSitesArr.length; i++) {
+    if (launchSitesArr[i].id != siteId) {
+      siteImg = siteImg;
+    } else {
+      siteImg = launchSitesArr[i].img;
+    }
+  };
   var crew = "";
-
-  var time = DateTime.fromISO(date).toLocaleString(DateTime.DATETIME_SHORT);
-  console.log(time);
-
   if (data.crew.length == 0) {
     crew = "N/A";
   } else if (data.crew.length > 0) {
     for (var i = 0; i < data.crew.length; i++) {
       crew = data.crew.join(", ");
     }
-  }
-
-  var launchInfoEl = document.createElement("h2");
-  launchInfoEl.setAttribute("style", "color:blue; padding-bottom:15px;");
-  launchInfoEl.textContent = "Next Launch:";
-  upcomingEl.appendChild(launchInfoEl);
-  var dateEl = document.createElement("p");
-  dateEl.innerHTML = "<strong style='color:blue;'>Launch date:</strong> " + date;
-  upcomingEl.appendChild(dateEl);
-  var flightEl = document.createElement("p");
-  flightEl.innerHTML = "<strong style='color:blue;'>Flight number:</strong> " + flight;
-  upcomingEl.appendChild(flightEl);
-  var crewEl = document.createElement("p");
-  crewEl.innerHTML = "<strong style='color:blue;'>Crew members:</strong> " + crew;
-  upcomingEl.appendChild(crewEl);
-
-  var launchpadId = data.launchpad;
-  var launchpadUrl = "https://api.spacexdata.com/v4/launchpads/" + launchpadId;
-  var launchSiteLink = "";
-  var launchSiteImg = "";
-  var payloadId = data.payloads[0];
-  var payloadUrl = "https://api.spacexdata.com/v4/payloads/" + payloadId;
-
-  for (var i = 0; i < launchSitesArr.length; i++) {
-    if (launchSitesArr[i].id != launchpadId) {
-      launchSiteLink = launchSiteLink;
-      launchSiteImg = launchSiteImg;
-    } else if (launchSitesArr[i].id == launchpadId) {
-      launchSiteLink = launchSitesArr[i].link;
-      launchSiteImg = launchSitesArr[i].img;
-    } else {
-      launchSiteLink = "./index.html"
-    }
-  }
+  };
 
   fetch(payloadUrl).then(function (response) {
     response.json().then(function (payloadData) {
-      var payload = payloadData.name;
+      let payload = payloadData.name;
+    });
+  });
 
-      var payloadEl = document.createElement("p");
-      payloadEl.innerHTML = "<strong style='color:blue;'>Payload:</strong> " + payload;
-      upcomingEl.appendChild(payloadEl);
-      var redditEl = document.createElement("a");
-      redditEl.setAttribute("href", reddit);
-      redditEl.setAttribute("target", "_blank");
-      redditEl.textContent = "Mission discussion (reddit)";
-      upcomingEl.appendChild(redditEl);
-
-      fetch(launchpadUrl).then(function (response) {
-        response.json().then(function (launchpadData) {
-    
-          var launchpad = launchpadData.full_name;
-          // var lat = launchpadData.latitude;
-          // var lon = launchpadData.longitude;
-    
-          var launchpadEl = document.createElement("p");
-          launchpadEl.innerHTML = "<p><a href='#" + launchSiteLink + "' style='text-decoration:none;'><strong style='color:blue;'>Launch site: </strong>" + launchpad + "</p>";
-          upcomingEl.appendChild(launchpadEl);
-          var imgEl = document.createElement("img");
-          imgEl.setAttribute("src", launchSiteImg);
-          // style settings are temporary
-          imgEl.setAttribute("style", "width:40%; border:1px solid blue; border-radius:4px;");
-          upcomingEl.appendChild(imgEl);
-        });
-      });
+  fetch(siteUrl).then(function (response) {
+    response.json().then(function (launchpadData) {
+      let launchpad = launchpadData.full_name;
     });
   });
 };
